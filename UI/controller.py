66acd1ab6fbbea58f7917ea31927ee870d5db669
DanieleFerri00 <s269085@studenti.polito.it>
2026-06-29 12:33:38 +0200

    Soluzione d'esame

--- UI/controller.py
+++ UI/controller.py
@@ -8,11 +8,49 @@ class Controller:
         # the model, which implements the logic of the program and holds the data
         self._model = model
 
+
     def fillDDsRating(self):
-        pass
+        ratings = self._model.getRatings()
+        for voto in ratings:
+            self._view._ddrating1.options.append(ft.dropdown.Option(voto))
+            self._view._ddrating2.options.append(ft.dropdown.Option(voto))
+        self._view.update_page()
 
-    def handleCreaGrafo(self, e):
-        pass
 
-    def handleCammino(self, e):
-        pass
+    def handleCreaGrafo(self,e):
+        self._model.buildGraph(self._view._ddrating1.value, self._view._ddrating2.value)
+        self._view.txt_result.controls.clear()
+        self._view.txt_result.controls.append(ft.Text("Grafo correttamente creato:"))
+        self._view.txt_result.controls.append(ft.Text(f"Numero di nodi:{self._model.getNumNodi()}"))
+        self._view.txt_result.controls.append(ft.Text(f"Numero di archi:{self._model.getNumEdges()}"))
+
+        topEdges = self._model.getTop5Edges()
+
+        self._view.txt_result.controls.append(ft.Text("Top 5 archi:"))
+
+        for u, v, data in topEdges:
+            self._view.txt_result.controls.append(
+                ft.Text(f"{u.Name} -> {v.Name} : {data['weight']}"))
+
+        self._view.update_page()
+
+        components, largest = self._model.getConnectedComponents()
+
+        self._view.txt_result.controls.append(ft.Text(f"Il grafo ha {len(components)} componenti connesse"))
+
+        self._view.txt_result.controls.append(ft.Text(f"La più grande componente connessa è lunga {len(largest)}:"))
+
+        for u in largest:
+            self._view.txt_result.controls.append(
+                ft.Text(f"{u.Name}"))
+
+        self._view.update_page()
+
+    def handleCammino(self,e):
+        bestPath = self._model.getBestPath()
+        self._view.txt_result.controls.append(ft.Text(f"Cammino massimo trovato ({len(bestPath)} nodi):"))
+
+        for a in bestPath:
+            self._view.txt_result.controls.append(ft.Text(a.Name))
+
+        self._view.update_page()
